32300291d6c034c09bbf455b9d2d306f7589a11f
Christopher Pitt <cgpitt@gmail.com>
2024-04-25 09:49:32 +0200

    Initial commit

---
+++ src/ToolboxServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BenchmarksForLaravel\Toolbox;
+
+use Illuminate\Support\ServiceProvider;
+
+class ToolboxServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // TODO
+    }
+
+    public function boot()
+    {
+        // TODO
+    }
+}
